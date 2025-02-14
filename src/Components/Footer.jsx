a442365068cfb9994d0117174d486bcaf59c5bad
Subhadip Mandal <subhadipmandal714@gmail.com>
2025-02-14 22:08:39 +0530

    add my resume

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,18 +8,34 @@ import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-      <a
-        href="mailto:subhadipmandal98833@gmail.com"
-        className="fixed right-3 bottom-5 bg-red-400 p-3 rounded-full shadow-lg md:flex md:h-20 md:w-20 md:items-center md:justify-center"
-      >
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png"
-          alt="Gmail"
-          className="w-8 h-8 md:h-10 md:w-10"
-        />
-      </a>
-    </div>
+    <>
+      <div>
+        <a
+          href="mailto:subhadipmandal98833@gmail.com"
+          className="fixed right-3 bottom-5 bg-red-400 p-3 rounded-full shadow-lg md:flex md:h-20 md:w-20 md:items-center md:justify-center"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png"
+            alt="Gmail"
+            className="w-8 h-8 md:h-10 md:w-10 2xl:h-12 2xl:w-12"
+          />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://drive.google.com/file/d/1B5JMvroZtEyIyxbZekWTLIiP23NMsEb_/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fixed right-3 bottom-20 md:bottom-32 bg-blue-400 p-3 rounded-full shadow-lg md:flex md:h-20 md:w-20 md:items-center md:justify-center"
+        >
+          <img
+            src="https://icons.veryicon.com/png/o/miscellaneous/general-icon-library/resume-7.png"
+            alt="Resume"
+            className="w-8 h-8 md:h-10 md:w-10 2xl:h-12 2xl:w-12"
+          />
+        </a>
+      </div>
+    </>
   );
 };
 
